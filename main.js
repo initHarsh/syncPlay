0c72a1b6b51971202d013e1cd7c9d0c149560843
initHarsh <harshdhillon.info@gmail.com>
2022-12-11 09:25:14 +0530

    removed unnecessary modules and  added start script

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 //   Modules require 
 const express=require('express')
 const app=express()
-const fileupload=require('express-fileupload')
-const terminal=require('child_process')
-const fs=require('fs')
-const https=require('https')
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -13,19 +9,11 @@ const io = new Server(server);
 // Project Global variables 
 var version='1.0.0'
 var port=3000
-var storage=__dirname+'/received'
-var animelist=[]
 // server modules to use
 
-
-
 app.use(express.urlencoded({extended:true}))
-
 app.use(express.static(__dirname+'/'))
 
-
-
-var userCount=0
 var users=[]
 
 io.on('connection', (socket) => {
@@ -72,44 +60,9 @@ io.on('connection', (socket) => {
 });
 
 
-
-
-
-
-
-
-// fs.readFile('anime.txt','utf-8',(e,o)=>{
-//      if (e) {
-//       console.log(e)
-//      }else{
-//         console.log('Loaded list ******* * ** * * *')
-//         animelist=JSON.parse(o)
-//       }
-// })
-
-
-
-
 app.get('/',(req,res)=>{
-	
 	res.sendFile(__dirname+'/index.html')
 })
-
-// app.post('/create',(req,res)=>{	
-
-// 	var roomId= Math.floor(100000 + Math.random() * 900000)
-// 	var roomFile=roomId+'.txt'
-
-// 	fs.writeFile(__dirname+"/rooms/"+roomFile,Date.now(),(e)=>{
-//         if (e) {console.log(e)}
-//           else {
-            
-//             console.log('registered profile')
-//           }
-//        })
-// 	res.send({url:'/ok'})
-// })
-
 
 app.get('/ok',(req,res)=>{
 	res.send('ok')
